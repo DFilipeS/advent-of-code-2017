2a5412245898e5b7aae5db629aad575126a2470d
Daniel Silva <dfilipes@protonmail.com>
2017-12-13 21:30:09 +0000

    Finished Day2.

--- day2/lib/day2.ex
+++ day2/lib/day2.ex
@@ -1,16 +1,45 @@
 defmodule Day2 do
-  
   def checksum(spreadsheet) do
     spreadsheet
     |> String.split("\n")
     |> Enum.filter(fn x -> x != "" end)
     |> Enum.map(fn row ->
-      numbers = 
+      numbers =
         row
         |> String.split(" ")
         |> Enum.map(fn x -> elem(Integer.parse(x), 0) end)
       Enum.max(numbers) - Enum.min(numbers)
     end)
     |> Enum.sum()
+  end
+
+  def checksum2(spreadsheet) do
+    spreadsheet
+    |> String.split("\n")
+    |> Enum.filter(fn x -> x != "" end)
+    |> Enum.map(fn row ->
+      numbers =
+        row
+        |> String.split(" ")
+        |> Enum.map(fn x -> elem(Integer.parse(x), 0) end)
+        |> find_even_division()
+    end)
+    |> Enum.sum()
+  end
+
+  defp find_even_division(numbers) do
+    numbers = Enum.with_index(numbers)
+
+    Enum.map(numbers, fn {x, index} ->
+      Enum.reduce(numbers, 0, fn ({y, index}, acc) ->
+        max = Enum.max([x, y])
+        min = Enum.min([x, y])
+        cond do
+          x != y and rem(max, min) == 0 -> div(max, min)
+          true -> acc
+        end
+      end)
+    end)
+    |> Enum.max()
   end
 end
--- day2/test/day2_test.exs
+++ day2/test/day2_test.exs
@@ -7,7 +7,17 @@ defmodule Day2Test do
     7 5 3
     2 4 6 8
     """
-    
+
     assert Day2.checksum(input) == 18
+  end
+
+  test "checksum2" do
+    input = """
+    5 9 2 8
+    9 4 7 3
+    3 8 6 5
+    """
+
+    assert Day2.checksum2(input) == 9
   end
 end
