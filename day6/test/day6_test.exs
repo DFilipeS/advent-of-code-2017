a1b998e301568020fd6b294f277a3c7006b56e7f
Daniel Silva <dfilipes@protonmail.com>
2017-12-17 18:30:14 +0000

    Finished Day6.

---
+++ day6/test/day6_test.exs
@@ -0,0 +1,24 @@
+defmodule Day6Test do
+  use ExUnit.Case
+  doctest Day6
+
+  describe "test find_cycle/1" do
+    test "with string input that takes 5 steps to detect the cycle" do
+      assert Day6.find_cycle("0 2 7 0") == 5
+    end
+
+    test "with input that takes 5 steps to detect the cycle" do
+      assert Day6.find_cycle([0, 2, 7, 0]) == 5
+    end
+  end
+
+  describe "test find_cycle_period/1" do
+    test "with string input where period of the first cycle is 4" do
+      assert Day6.find_cycle_period("0 2 7 0") == 4
+    end
+
+    test "with input where period of the first cycle is 4" do
+      assert Day6.find_cycle_period([0, 2, 7, 0]) == 4
+    end
+  end
+end
